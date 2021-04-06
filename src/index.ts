65592497c2d3be1f8c1b710973a20713df4fc570
Stasrikan <rubtid228@gmail.com>
2021-04-06 19:10:03 +0300

    Added return types to JSendSerializer class methods.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
+import {
+  JSendErrorResponse,
+  JSendFailResponse,
+  JSendSuccessResponse,
+} from "./JSendResponseTypes";
+
+/*====================*/
+
 class JSendSerializer {
-  public successResponse(data: any) {
+  public successResponse(data: any): JSendSuccessResponse<typeof data> {
     return this.response("succees", data);
   }
 
-  public failResponse(errors: any) {
+  public failResponse(errors: any): JSendFailResponse<typeof errors> {
     return this.response("fail", errors);
   }
 
-  public errorResponse(message: string) {
+  public errorResponse(message: string): JSendErrorResponse {
     return this.response("error", message);
   }
 
